54c96e330e9bb1bd2f303491dcdb4482a4960bbe
Alok-Sharma <alok.sharma127@gmail.com>
2015-04-07 02:21:27 -0400

    Added first few methods to Fibonacci heap. Incomplete implementation of removeMin

--- src/aloksharma/ads/part1/FibonacciHeap.java
+++ src/aloksharma/ads/part1/FibonacciHeap.java
@@ -1,5 +1,76 @@
 package aloksharma.ads.part1;
 
 public class FibonacciHeap {
+	
+	Node minNode = null;
+	int heapSize = 0;
+	
+	public Node enqueue(int data, int priority){
+		Node newNode = new Node(data, priority);
+		minNode = mergeLists(minNode, newNode);
+		heapSize++;
+		return newNode;
+	}
+	
+	public Node getMin(){
+		return minNode;
+	}
+	
+	public int getSize(){
+		return heapSize;
+	}
+	
+	public Node removeMin(){
+		if(minNode == null)
+			return null;
+		
+		Node minCopy = minNode;
+		heapSize--;
+		
+		//case 1, min node is alone.
+		if(minNode.next == minNode){
+			minNode = null; //remove it.
+		}
+		//case 2, connect the neighbours of min node together.
+		else{ 
+			minNode.prev.next = minNode.next;
+			minNode.next.prev = minNode.prev;
+			minNode = minNode.next;
+		}
+		//at this point minNode is some arbit sibling of the min element, in the topmost list.
+		
+		//clear parent field of all children of minNode.
+		if(minCopy.child != null){
+			Node child = minCopy.child;
+			do{
+				child.parent = null;
+				child = child.next;
+			}while(child != minCopy.child);
+		}
+		
+		minNode = mergeLists(minNode, minCopy.child);
+		
+		
+	}
+	
+	private Node mergeLists(Node one, Node two){
+		if(one == null && two == null){
+			return null;
+		}else if(one == null && two != null){
+			return two;
+		}else if(one != null && two == null){
+			return one;
+		}else{
+			//both aren't null, merge them.
+			Node oneNext = one.next;
+			one.next = two.next;
+			one.next.prev = one;
+			two.next = oneNext;
+			two.next.prev = two;
+			
+			return one.getPriority() < two.getPriority() ? one : two;
+		}
+	}
+	
 
 }
--- src/aloksharma/ads/part1/Node.java
+++ src/aloksharma/ads/part1/Node.java
@@ -1,15 +1,51 @@
 package aloksharma.ads.part1;
 
 public class Node {
-	private int Data;
-	
-	private Node left;
-	private Node right;
-	private Node parent;
-	private Node child;
-	
-	private Boolean childCut;
+	private int data;
+	private int degree;
 	private int priority;
 	
+	public Node prev;
+	public Node next;
+	public Node parent;
+	public Node child;
+	
+	private Boolean childCut;
+	
+	public Node(int data, int priority){
+		this.data = data;
+		this.childCut = false;
+		this.priority = priority;
+	}
+	
+	/*
+	 * Setter methods.
+	 */
+	public void setPriority(int priority){
+		this.priority = priority;
+	}
+	
+	public void setData(int data){
+		this.data = data;
+	}
+	
+	public void setChildCut(Boolean childCut){
+		this.childCut = childCut;
+	}
+	
+	/*
+	 * Getter methods
+	 */
+	public int getData(){
+		return data;
+	}
+	
+	public boolean isChildCut(){
+		return childCut;
+	}
+	
+	public int getPriority(){
+		return priority;
+	}
 	
 }
